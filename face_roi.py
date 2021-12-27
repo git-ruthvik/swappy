d1d3a7f6693fdc39eb6c38e3f50aa6d621523358
git-ruthvik <ruthvik.b.r@gmail.com>
2021-12-28 02:14:34 +0530

    face_roi algorithm changed, main code updated

--- face_roi.py
+++ face_roi.py
@@ -2,33 +2,22 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# img = cv2.imread('img.jpg')
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-# for faces in faces:
-#     x, y, w, h = faces
-#     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-#     if faces == ():
-#         print('No faces found')
-#     else:
-#         roi = img[y-50:y+h+100, x-100:x+w+100]
-#         cv2.imshow('roi', roi)
-# cv2.imshow('img', img)
-# cv2.waitKey(0)
-
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 6)
     for faces in faces:
         x, y, w, h = faces
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        if faces == ():
+        if faces != ():
+            if y >= 100 and x >= 100:
+                roi = frame[y - 50:y + h + 100, x - 100:x + w + 100]
+            elif y <= 100 or x <= 100:
+                roi = frame
+        elif faces == ():
             print('No faces found')
-        else:
-            roi = frame[y - 50:y + h + 100, x - 100:x + w + 100]
-            cv2.imshow('roi', roi)
+    cv2.imshow('roi', roi)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- main.py
+++ main.py
@@ -7,37 +7,33 @@
 
 
 import cv2
-
 cap1 = cv2.VideoCapture(0)
 cap2 = cv2.VideoCapture(2)
 cap3 = cv2.VideoCapture(1)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
 while True:
-    ret1, frame1 = cap1.read()
-    ret2, frame2 = cap2.read()
-    ret3, frame3 = cap3.read()
-    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    gray3 = cv2.cvtColor(frame3, cv2.COLOR_BGR2GRAY)
-    faces1 = face_cascade.detectMultiScale(gray1, 1.3, 5)
-    faces2 = face_cascade.detectMultiScale(gray2, 1.3, 5)
-    faces3 = face_cascade.detectMultiScale(gray3, 1.3, 5)
-    show_frame = frame1
-    if faces1 != () and faces2 != () and faces3 != ():
-        show_frame = frame1
-    elif faces1 != () and faces2 == () and faces3 == ():
-        show_frame = frame1
-    elif faces1 == () and faces2 == () and faces3 != ():
-        show_frame = frame3
-    elif faces1 == () and faces2 != () and faces3 == ():
-        show_frame = frame2
+    ret1, cam1 = cap1.read()
+    ret2, cam2 = cap2.read()
+    ret3, cam3 = cap3.read()
+    gray1 = cv2.cvtColor(cam1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(cam2, cv2.COLOR_BGR2GRAY)
+    gray3 = cv2.cvtColor(cam3, cv2.COLOR_BGR2GRAY)
+    faceincam1 = face_cascade.detectMultiScale(gray1, 1.3, 5)
+    faceincam2 = face_cascade.detectMultiScale(gray2, 1.3, 5)
+    faceincam3 = face_cascade.detectMultiScale(gray3, 1.3, 5)
+    show_frame = cam1
+    if faceincam1 == () and faceincam2 == () and faceincam3 == ():
+        show_frame = cam1
+    elif faceincam1 != () and faceincam2 == () and faceincam3 == ():
+        show_frame = cam1
+    elif faceincam1 == () and faceincam2 == () and faceincam3 != ():
+        show_frame = cam3
+    elif faceincam1 == () and faceincam2 != () and faceincam3 == ():
+        show_frame = cam2
 
     cv2.imshow('Final', show_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-
 cap1.release()
 cap2.release()
 cap3.release()
